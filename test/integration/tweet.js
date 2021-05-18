d2303e45625096ba0c615202cd0cce79b5f72181
DumasOlivier <dumas.olivier@outlook.fr>
2021-05-18 16:26:25 +0800

    Testing Tweet data

---
+++ test/integration/tweet.js
@@ -0,0 +1,20 @@
+const TweetStorage = artifacts.require('TweetStorage');
+
+contract('tweets', () => {
+  // Creating a tweet for testing.
+  before(async () => {
+    const tweetStorage = await TweetStorage.deployed();
+    await tweetStorage.createTweet(1, 'Hello world!');
+  });
+
+  it('can get tweet', async () => {
+    const storage = await TweetStorage.deployed();
+
+    const tweet = await storage.tweets.call(1);
+    const { id, text, userId } = tweet;
+
+    assert.equal(parseInt(id), 1);
+    assert.equal(text, 'Hello world!');
+    assert.equal(parseInt(userId), 1);
+  });
+});
