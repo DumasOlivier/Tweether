b0bb9a3ec8ab8ca588d32b28cc14e6c2fc37007f
DumasOlivier <dumas.olivier@outlook.fr>
2021-05-21 14:57:30 +0800

    get user's wallet and balance

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { eth } from '../web3/provider';
 
 const IndexPage = () => {
+  // Hooks
+  useEffect(() => {
+    try {
+      // Prompt user to let our DApp access their addresses
+      enableEthereum();
+      getEthAddress().then((addresses) => {
+        console.log('addresses', addresses);
+        getBalance(addresses).then((value) => {
+          console.log('value', value);
+        });
+      });
+    } catch (e) {
+      console.log(e, 'User denied access to their ETH addresses!');
+    }
+  }, []);
+
+  // Functions
+  const enableEthereum = async () => {
+    try {
+      await ethereum.enable();
+    } catch (e) {
+      console.log('e', e);
+    }
+  };
+
+  const getEthAddress = async () => {
+    // Get user's ETH addresses
+    try {
+      return await eth.getAccounts();
+    } catch (e) {
+      console.log('e', e);
+    }
+  };
+
+  const getBalance = async (addresses) => {
+    return await eth.getBalance(addresses[0]);
+  };
+
   return <h1>Hello world from Tweether</h1>;
 };
 
